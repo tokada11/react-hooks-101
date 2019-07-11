d35389b3c383624638d7addf0486332b60f415c1
tsokadance@gmail.com <tsokadance@gmail.com>
2019-07-12 00:18:51 +0900

    hello react hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  const reset = () => setCount(0)
+  const double = () => setCount(count*2)
+  const third = () => setCount((count%3 === 0) ? count/3 : count)
   return (
-    <div>
-      This is a template for React app.
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={third}>かつらさんど</button>
+      </div>
+    </>
   )
 }
 
