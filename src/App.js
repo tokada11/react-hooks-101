61507b790322ab182dfc9d68c51a5a8599465bc6
tsokadance@gmail.com <tsokadance@gmail.com>
2019-07-13 11:39:21 +0900

    impliment component using nultiplu states

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount(count*2)
-  const third = () => setCount((count%3 === 0) ? count/3 : count)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={third}>かつらさんど</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price+1)}>+1</button>
+      <button onClick={() => setPrice(price-1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
