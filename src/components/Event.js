89c675cdebad8c826a99c8eea2a4558a5bee1c8e
tsokadance@gmail.com <tsokadance@gmail.com>
2019-07-20 15:03:57 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT} from '../actions'
+import {
+    ADD_OPERATION_LOG,
+    DELETE_EVENT
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const Event = ({ event }) => {
     const {dispatch} = useContext(AppContext)
@@ -12,6 +16,11 @@ const Event = ({ event }) => {
         dispatch({
             type: DELETE_EVENT,
             id
+        })
+        dispatch({
+            type: ADD_OPERATION_LOG,
+            description: `イベント(id=${id})を削除しました。`,
+            operatedAt: timeCurrentIso8601()
         })
       }
     }
